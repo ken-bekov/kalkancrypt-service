8e08158bdc637accd9b4848d180233d786e53e05
Saken Sultanbekov <saken@mail.ru>
2023-05-07 19:10:45 +0600

    fix kalkan libs path

--- libs/add-to-maven.sh
+++ libs/add-to-maven.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-mvn install:install-file -Dfile=kalkancrypt-0.7.2.jar -DgroupId=kz.egov -DartifactId=kalkancrypt -Dversion=0.7.2 -Dpackaging=jar
-mvn install:install-file -Dfile=knca_provider_util-0.8.jar -DgroupId=kz.egov -DartifactId=kalkancrypt_util -Dversion=0.8 -Dpackaging=jar
+mvn install:install-file -Dfile=libs/kalkancrypt-0.7.2.jar -DgroupId=kz.egov -DartifactId=kalkancrypt -Dversion=0.7.2 -Dpackaging=jar
+mvn install:install-file -Dfile=libs/knca_provider_util-0.8.jar -DgroupId=kz.egov -DartifactId=kalkancrypt_util -Dversion=0.8 -Dpackaging=jar
